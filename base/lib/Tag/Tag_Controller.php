7d7c1833957a1fcd387ef80173e722cb4a33aaa0
Raul Leano Martinet <leanomartinet@MacBook-Pro-de-Raul.local>
2018-02-09 22:57:44 +0100

    Added the autocomplete tag

--- base/lib/Tag/Tag_Controller.php
+++ base/lib/Tag/Tag_Controller.php
@@ -1,5 +1,36 @@
 <?php
 class Tag_Controller extends Controller {
 
+	public function controlActions(){
+		switch ($this->action) {
+			default:
+				return parent::controlActions();
+			break;
+			case 'autocomplete':
+				$this->mode = 'json';
+                $autocomplete = (isset($_GET['term'])) ? $_GET['term'] : '';
+                if ($autocomplete!='') {
+                	$query = 'SELECT '.Db::prefixTable('Tag').'.idTag as idItem, '.Db::prefixTable('Tag').'.name as infoItem, COUNT(dir_Tag.idTag) as numItems
+                            FROM '.Db::prefixTable('Tag').'
+                            JOIN '.Db::prefixTable('PlaceTag').' ON '.Db::prefixTable('Tag').'.idTag='.Db::prefixTable('PlaceTag').'.idTag
+                            WHERE '.Db::prefixTable('Tag').'.name LIKE "%'.$autocomplete.'%"
+                            GROUP BY '.Db::prefixTable('Tag').'.idTag
+                            ORDER BY numItems DESC, '.Db::prefixTable('Tag').'.nameUrl
+                            LIMIT 20';
+                    $results = array();
+                    $resultsAll = Db::returnAll($query);
+                    foreach ($resultsAll as $result) {
+                        $resultsIns = array();
+                        $resultsIns['id'] = $result['idItem'];
+                        $resultsIns['value'] = $result['infoItem'];
+                        $resultsIns['label'] = $result['infoItem'].' ('.$result['numItems'].')';
+                        array_push($results, $resultsIns);
+                    }
+                    return json_encode($results);
+                }
+			break;
+		}
+	}
+
 }
 ?>
